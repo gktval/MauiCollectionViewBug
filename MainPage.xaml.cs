5c3f64b520dc1c7bde1e478118a7f2df32947150
Travis Yeik <tyeik@huskers.unl.edu>
2023-10-30 15:07:38 -0600

    first

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MauiApp6
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Layer> _layers;
+        public ObservableCollection<Layer> Layers
+        {
+            get { return _layers; }
+            set { _layers = value; OnPropertyChanged(nameof(Layers)); }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Layers = [new Layer("Item 1", "ABC"), new Layer("Item 2", "DEF"), new Layer("Item 3", "GHI"), new Layer("Item 4", "sdf")];
+            BindingContext = this;
+        }
+
+    }
+
+    public class Layer
+    {
+        public Layer(string name, string description)
+        {
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+}
